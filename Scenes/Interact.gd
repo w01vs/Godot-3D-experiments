0aae669b858719c414b671538dc12d0e4de697c4
zWolv <samoht684@gmail.com>
2023-10-18 22:42:56 +0200

    first commit

---
+++ Scenes/Interact.gd
@@ -0,0 +1,15 @@
+extends RayCast3D
+
+var current_collider
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	var collider := get_collider()
+	if is_colliding() and collider is Interactable:
+		if current_collider != collider:
+			current_collider = collider
+		
+		if(Input.is_action_just_pressed("interact")):
+			collider.interact()
+	elif current_collider:
+		current_collider = null

5efec917c33d40998e5efe3bf4991a56349a65f8
w01vs <57898164+w01vs@users.noreply.github.com>
2023-10-19 12:06:31 +0200

    healthbar + healthcomponent

--- Scenes/Interact.gd
+++ Scenes/Interact.gd
@@ -4,12 +4,15 @@ var current_collider
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
+	collision()
+		
+func collision() -> void:
 	var collider := get_collider()
 	if is_colliding() and collider is Interactable:
 		if current_collider != collider:
 			current_collider = collider
 		
 		if(Input.is_action_just_pressed("interact")):
-			collider.interact()
+			collider.interact(get_parent_node_3d().get_parent_node_3d().get_parent_node_3d().get_parent_node_3d())
 	elif current_collider:
 		current_collider = null
---
+++ Scripts/HealthComponent.gd
@@ -0,0 +1,40 @@
+extends Node
+class_name HealthComponent
+
+var health: float
+var max_health : float = 100
+
+signal health_changed(float)
+signal died()
+
+func _ready():
+	health = max_health
+
+func _process(delta: float) -> void:
+	clamp_health()
+	health_0()
+
+func decrease_health(amount: int) -> void:
+	if health > 0:
+		health -= amount
+		clamp_health()
+		health_changed.emit(health)
+
+func increase_health(amount: int) -> void:
+	if health < max_health:
+		health += amount
+		clamp_health()
+		health_changed.emit(health)
+
+func health_0() -> void:
+	if health <= 0:
+		died.emit()
+
+func clamp_health() -> void:
+	health = clamp(health, 0, max_health)
+	
+func set_max_health(amount: float) -> void:
+	max_health = amount
+	
+func get_max_health() -> float:
+	return max_health

3df3a47d03d73543831c52beba7ff91c95d6d02f
Wolv <samoht684@gmail.com>
2023-11-16 15:36:46 +0100

    enemy follow state

---
+++ Scripts/enemy_follow_state.gd
@@ -0,0 +1,27 @@
+extends State
+class_name EnemyFollowState
+
+@onready var actor: CharacterBody3D = $"../.."
+@onready var nav_agent: NavigationAgent3D = $"../../NavigationAgent3D"
+
+func _ready() -> void:
+	execute_in_process = false
+
+func _enter_tree() -> void:
+	pass
+
+func _exit_tree() -> void:
+	pass
+
+func _update(delta: float) -> void:
+	nav_agent.target_position = GlobalRefs.player.global_position
+	
+	var next_position = nav_agent.get_next_path_position()
+	var new_velocity = (next_position - global_position).normalized() * actor.SPEED
+	actor.velocity = actor.velocity.move_toward(new_velocity, .4)
+	actor.look_at(GlobalRefs.player.global_position)
+	
+	if nav_agent.is_target_reached():
+		actor.velocity = Vector3.ZERO
+		
+	actor.move_and_slide()

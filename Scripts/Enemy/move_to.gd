8600df996a19a2ab61b8938f2ad30435cd23efe6
Wolv <samoht684@gmail.com>
2023-11-20 17:38:10 +0100

    enemy looks where its going

--- Scripts/Enemy/move_to.gd
+++ Scripts/Enemy/move_to.gd
@@ -2,6 +2,8 @@ extends ActionNodeBT
 
 @onready var nav_agent: NavigationAgent3D = $"../../../../NavigationAgent3D"
 @onready var actor = $"../../../.."
+@onready var timer: Timer = $Timer
+var smooth_time_finished: bool = false
 
 func action() -> int:
 	if nav_agent.is_target_reached():
@@ -11,6 +13,9 @@ func action() -> int:
 	var next_position = nav_agent.get_next_path_position()
 	var new_velocity = (next_position - actor.global_position).normalized() * actor.SPEED
 	actor.velocity = actor.velocity.move_toward(new_velocity, .4)
+	var lookdir = atan2(-actor.velocity.x, -actor.velocity.z)
+	actor.rotation.y = lookdir
+	
 	
 	actor.move_and_slide()
 	return RUNNING

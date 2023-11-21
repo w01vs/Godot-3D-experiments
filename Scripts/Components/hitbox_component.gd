5a82f503355465af92d898fa1461fa01b191bacc
w01vs <57898164+w01vs@users.noreply.github.com>
2023-11-21 14:22:07 +0100

    some fixes

--- Scripts/Components/hitbox_component.gd
+++ Scripts/Components/hitbox_component.gd
@@ -7,6 +7,11 @@ func _ready() -> void:
 	set_collision_mask_value(1, false)
 	set_collision_layer_value(4, true)
 	set_collision_mask_value(3, true)
-	
+	connect("area_entered", _on_area_entered)
+
 func set_info(info: OnHitInformation) -> void:
 	on_hit_information = info
+
+func _on_area_entered(area: Area3D):
+	if area is HurtboxComponent:
+		UniversalHitController.apply_information_to(on_hit_information, area.get_parent())

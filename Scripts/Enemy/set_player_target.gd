09c256deeefe5d1d3cbc1baf33807e70d75d9257
Wolv <samoht684@gmail.com>
2023-11-21 23:26:41 +0100

    fiddled with enemy btree

---
+++ Scripts/Enemy/set_player_target.gd
@@ -0,0 +1,9 @@
+extends ActionNodeBT
+
+@export var actor: SimpleEnemy
+
+func action() -> int:
+	actor.nav_agent.target_position = GlobalRefs.player.global_position
+	if not actor.nav_agent.is_target_reachable():
+		return FAILED
+	return SUCCESS

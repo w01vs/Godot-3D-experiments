0dc2210fe4fd1027be1cf07057dc7f5e7eb236cc
w01vs <57898164+w01vs@users.noreply.github.com>
2024-02-10 02:36:17 +0100

    slash

--- Scripts/BehaviorTree/invert_bt_node.gd
+++ Scripts/BehaviorTree/invert_bt_node.gd
@@ -2,12 +2,10 @@ class_name InvertNodeBT extends BTNode
 
 func _ready() -> void:
 	get_bt_children()
-	if(children.size() > 1):
-		# Throw an error or something
-		pass
+	assert(children.size() == 1)
 
 func execute() -> int:
-	var res: int = children[0].execute()
+	var res: int = await children[0].execute()
 	if res == SUCCESS:
 		return FAILED
 	elif res == RUNNING:

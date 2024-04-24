e03e42f3705c34e85ce15fa3acda0ea0f7fcf39b
Wolv <samoht684@gmail.com>
2024-04-24 23:08:31 +0200

    removed some old files

---
+++ Resources/Items and UI representations/iv_item_template.gd
@@ -0,0 +1,7 @@
+class_name IVItemTemplate extends Resource
+
+@export var name: String
+@export var ui_texture: Texture2D
+@export var scene: PackedScene
+
+@export_multiline var description: String

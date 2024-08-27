cca5e1ec06024964ae4380544d665d2300964623
Wolv <samoht684@gmail.com>
2024-08-27 21:04:09 +0200

    i can place shit

--- Scripts/Interact.gd
+++ Scripts/Interact.gd
@@ -2,19 +2,21 @@ extends RayCast3D
 
 var current_collider: Interactable
 
-signal ui_switched
+signal ui_switched(state: GlobalRefs.PlayerState)
 
 @onready var player: Node3D = $"../../../.."
 
-enum States {DEFAULT = 0, BUILD = 1}
+var build_scene: PackedScene = load("res://Scenes/build_box.tscn")
 
-var state : States = States.DEFAULT
+var buildx: BuildBox = null
+
+var state : GlobalRefs.PlayerState = GlobalRefs.PlayerState.DEFAULT
 
 func _process(_delta: float) -> void:
 	match state:
-		States.DEFAULT:
+		GlobalRefs.PlayerState.DEFAULT:
 			collision()
-		States.BUILD:
+		GlobalRefs.PlayerState.BUILD:
 			pass
 
 func collision() -> void:
@@ -30,15 +32,49 @@ func collision() -> void:
 
 
 func _physics_process(delta: float) -> void:
-	match state:
-		States.DEFAULT: 
-			state = States.BUILD
-			ui_switched.emit(state)
-			if Input.is_action_just_pressed("build"):
+	if Input.is_action_just_pressed("build"):
+		match state:
+			GlobalRefs.PlayerState.DEFAULT: 
+				state = GlobalRefs.PlayerState.BUILD
+				ui_switched.emit(state)
 				target_position.y = -15
-			# instance an object and position it at the intersection point of the ray.
-		States.BUILD:
-			state = States.DEFAULT
-			ui_switched.emit(state)
-			if Input.is_action_just_pressed("build"):
+				set_collision_mask_value(2, false)
+				set_collision_mask_value(1, true)
+				set_collision_mask_value(5, true)
+				buildx = build_scene.instantiate()
+				GlobalRefs.world.add_child(buildx)
+				if is_colliding():
+					buildx.visible = true
+					buildx.global_position = get_collision_point()
+					buildx.global_position = Vector3(buildx.global_position.x, buildx.global_position.y + buildx.scale.y / 2, buildx.global_position.z)
+					buildx.to_holo()
+				else:
+					buildx.visible = false
+					
+				# instance an object and position it at the intersection point of the ray.
+			GlobalRefs.PlayerState.BUILD:
+				state = GlobalRefs.PlayerState.DEFAULT
+				ui_switched.emit(state)
+				set_collision_mask_value(2, true)
+				set_collision_mask_value(1, false)
+				set_collision_mask_value(5, false)
+				if buildx:
+					buildx.queue_free()
+					buildx = null
 				target_position.y = -5
+				
+	match state:
+		GlobalRefs.PlayerState.BUILD:
+			if is_colliding():
+				buildx.visible = true
+				buildx.to_holo()
+				buildx.global_position = get_collision_point()
+				buildx.global_position = Vector3(buildx.global_position.x, buildx.global_position.y + buildx.scale.y / 2, buildx.global_position.z)
+				if Input.is_action_just_pressed("default_attack"):
+					buildx.place()
+					buildx = build_scene.instantiate()
+					GlobalRefs.world.add_child(buildx)
+					buildx.to_holo()
+			else:
+				buildx.visible = false
+				
--- Scripts/world.gd
+++ Scripts/world.gd
@@ -2,3 +2,4 @@ class_name World extends Node3D
 
 func _ready() -> void:
 	GlobalRefs.map = get_world_3d().navigation_map
+	GlobalRefs.world = self

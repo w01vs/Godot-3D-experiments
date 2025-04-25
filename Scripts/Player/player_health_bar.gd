ed2d2e175697e918fb76b83a228a6851ba6fb48b
w01vs <57898164+w01vs@users.noreply.github.com>
2025-04-25 14:04:29 +0200

    fixed querying navserver before first sync

--- Scripts/Player/player_health_bar.gd
+++ Scripts/Player/player_health_bar.gd
@@ -1,6 +1,5 @@
 class_name BetterHealthBar extends ProgressBar
 
-var tween: Tween = create_tween()
 @onready var animator: AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
@@ -8,5 +7,3 @@ func _ready() -> void:
 
 func animate(new_value: float) -> void:
 	animator.play("twinkle")
-	#var duration = animator.current_animation_length
-	#tween.interpolate_value(value,  new_value - value, 0, duration, Tween.TRANS_ELASTIC, Tween.EASE_OUT)

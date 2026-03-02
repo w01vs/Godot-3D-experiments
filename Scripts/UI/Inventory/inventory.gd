4039cba768f3e336129e61713c8c19f6fcfc591e
w01vs <57898164+w01vs@users.noreply.github.com>
2026-03-02 17:24:34 +0100

    implemented dragging items in inventory

--- Scripts/UI/Inventory/inventory.gd
+++ Scripts/UI/Inventory/inventory.gd
@@ -14,4 +14,6 @@ func populate_inv(slots: Array[SlotData]) -> void:
 		var slot = Slot.instantiate()
 		item_grid.add_child(slot)
 		if slot_data:
+			print_debug(slot_data.item_data.texture)
 			slot.set_data(slot_data)
+			slot.update_ui()

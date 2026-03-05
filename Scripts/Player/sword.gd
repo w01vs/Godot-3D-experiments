c61d3e82eeb3d3667a851eb4ce167f060fd91b72
w01vs <57898164+w01vs@users.noreply.github.com>
2026-03-05 23:25:32 +0100

    split inventory ui/data

--- Scripts/Player/sword.gd
+++ Scripts/Player/sword.gd
@@ -10,3 +10,17 @@ func _ready():
 
 func use() -> void:
 	pass
+
+func on_animation_trigger(event: String) -> void:
+	match event:
+		"hitbox_on":
+			hitbox.monitoring = true
+
+func on_animation_start() -> void:
+	pass
+
+func on_animation_end() -> void:
+	hitbox.monitoring = false
+
+func _get_animation() -> String:
+	return "slash_attack"

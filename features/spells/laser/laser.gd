54c89693dbbb44296707ea28fa06193ba2af1b1f
w01vs <57898164+w01vs@users.noreply.github.com>
2026-08-04 15:29:20 +0200

    laser changes length based on colliding

--- features/spells/laser/laser.gd
+++ features/spells/laser/laser.gd
@@ -1,4 +1,4 @@
-class_name Laser extends Node3D
+class_name Laser extends CRayCast3D
 
 @export var anim: AnimationPlayer
 var cooldown: bool
@@ -6,6 +6,9 @@ var cooldown: bool
 @export var beam_in: GPUParticles3D
 @export var beam_cap: GPUParticles3D
 @export var fade: MeshInstance3D
+
+var default_length: float = 15
+var max_length: float = 15
 
 func _enter_tree() -> void:
 	var mat: ShaderMaterial = fade.material_override
@@ -16,6 +19,26 @@ func _ready() -> void:
 	beam_out.emitting = false
 	beam_in.emitting = false
 	beam_cap.emitting = false
+
+func _physics_process(_delta: float) -> void:
+	if cooldown:
+		var mat: ParticleProcessMaterial = beam_in.process_material as ParticleProcessMaterial
+		var mat2: ParticleProcessMaterial = beam_out.process_material as ParticleProcessMaterial
+		if is_colliding():
+			var target: Vector3 = get_collision_point()
+			var length: float = abs((global_position - target).length())
+			var percent_len: float = length / default_length
+			beam_cap.global_position = target
+			mat.scale_3d_max.z = percent_len
+			mat.scale_3d_min.z= percent_len
+			mat2.scale_3d_max.z = percent_len
+			mat2.scale_3d_min.z= percent_len
+		else:
+			beam_cap.position = Vector3(0, 0, -default_length)
+			mat.scale_3d_max.z = 1
+			mat.scale_3d_min.z = 1
+			mat2.scale_3d_max.z = 1
+			mat2.scale_3d_min.z = 1
 
 func end(_name: StringName) -> void:
 	cooldown = false
